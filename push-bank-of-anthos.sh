6dddd38268954e6815b36f2bcd57fe5ffefb2068
Mike Laramie <mike.laramie@lacework.net>
2022-01-21 09:42:26 -0500

    updated push scripts for ECR

--- push-bank-of-anthos.sh
+++ push-bank-of-anthos.sh
@@ -1,14 +1,21 @@
 #! /bin/zsh
-region="us-east1"
-project_id="mikelaramie-artifacts"
-version="v0.5.0"
+aws_account="100588478000"
+region="us-west-2"
+#project_id="mikelaramie-artifacts"
+version="v0.5.3"
+
+aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account}.dkr.ecr.${region}.amazonaws.com
 
 for container (accounts-db balancereader contacts frontend ledger-db ledgerwriter loadgenerator transactionhistory userservice); do
-docker pull gcr.io/bank-of-anthos/${container}:${version}
-docker tag gcr.io/bank-of-anthos/${container}:${version} ${region}-docker.pkg.dev/${project_id}/bank-of-anthos/${container}:${version}
-docker push ${region}-docker.pkg.dev/${project_id}/bank-of-anthos/${container}:${version}
+docker pull gcr.io/bank-of-anthos-ci/${container}:${version}
+aws ecr create-repository --repository-name bank-of-anthos/${container} --region ${region}
+docker tag gcr.io/bank-of-anthos-ci/${container}:${version} ${aws_account}.dkr.ecr.${region}.amazonaws.com/bank-of-anthos/${container}:${version}
+#docker tag gcr.io/bank-of-anthos/${container}:${version} ${region}-docker.pkg.dev/${project_id}/bank-of-anthos/${container}:${version}
+docker push ${aws_account}.dkr.ecr.${region}.amazonaws.com/bank-of-anthos/${container}:${version}
+#docker push ${region}-docker.pkg.dev/${project_id}/bank-of-anthos/${container}:${version}
 done
 
 #for container (accounts-db balancereader contacts frontend ledger-db ledgerwriter loadgenerator transactionhistory userservice); do
 #lw vuln ctr scan us-east1-docker.pkg.dev mikelaramie-artifacts/bank-of-anthos/${container} v0.5.0
 #done
+
--- push-microservices-demo.sh
+++ push-microservices-demo.sh
@@ -1,9 +1,14 @@
 #! /bin/zsh
-registry="q5y5l4i3"
-version="v0.2.4"
+#aws_account="100588478000"
+gcp_project="mikelaramie-artifacts-2"
+region="us"
+version="v0.3.5"
 
 for container (adservice cartservice checkoutservice currencyservice emailservice frontend loadgenerator paymentservice productcatalogservice recommendationservice shippingservice ); do
 docker pull gcr.io/google-samples/microservices-demo/${container}:${version}
-docker tag gcr.io/google-samples/microservices-demo/${container}:${version} public.ecr.aws/${registry}/${container}:${version}
-docker push public.ecr.aws/${registry}/${container}:${version}
+#docker tag gcr.io/google-samples/microservices-demo/${container}:${version} ${aws_account}.dkr.ecr.${region}.amazonaws.com/microservices-demo/${container}:${version}
+docker tag gcr.io/google-samples/microservices-demo/${container}:${version} ${region}-docker.pkg.dev/${gcp_project}/online-boutique/${container}:${version}
+#docker push ${aws_account}.dkr.ecr.${region}.amazonaws.com/microservices-demo/${container}:${version}
+docker push ${region}-docker.pkg.dev/${gcp_project}/online-boutique/${container}:${version}
+
 done
