8f56c2593f31f1f76dc6f7776da2391c3e8af47d
Mike Laramie <mike.laramie@lacework.net>
2022-04-05 15:52:45 -0400

    updating for gcr

--- configure-bank-of-anthos.sh
+++ configure-bank-of-anthos.sh
@@ -1,12 +1,19 @@
 #!/bin/zsh
 
+## AWS ECR
+#aws_account="100588478000"
+#region="us-west-2"
+#docker_repo="${aws_account}.dkr.ecr.${region}.amazonaws.com/bank-of-anthos/"
+
+## GCP GAR
 #region="us-east1"
 #project_id="gke-project-01-05c8c7d8"
-#docker-repo="${region-docker}.pkg.dev/${project_id}"
-aws_account="100588478000"
-region="us-west-2"
-docker_repo="${aws_account}.dkr.ecr.${region}.amazonaws.com/bank-of-anthos/"
+#docker-repo="${region}-docker.pkg.dev/${project_id}"
 
+## GCP GCR
+region="us"
+project_id="gke-polygraph-demo"
+docker-repo="${region}.gcr.io/${project_id}"
 
 git clone git@github.com:GoogleCloudPlatform/bank-of-anthos.git
 
