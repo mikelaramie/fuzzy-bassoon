3318114257eb3d20e0a0db1ff92e92c9205451dd
Mike Laramie <mike.laramie@lacework.net>
2022-04-06 11:59:36 -0400

    Added more robust logic for ECR/GAR/GCR to BOA/OB scripts

--- push-microservices-demo.sh
+++ push-microservices-demo.sh
@@ -1,26 +1,15 @@
 #! /bin/zsh
-#AWS
-aws_account="100588478000"
-aws_region="us-west-2"
+source repositories.sh
 
-#GCP
-gcp_project_id="gke-polygraph-demo"
-gcp_gar_region="us" 
-gcp_gcr_region="us." # must end in . for regional GCR; leave blank for global `gcr.io`
-
-# The version of images from https://github.com/GoogleCloudPlatform/microservices-demo/blob/main/release/kubernetes-manifests.yaml
-source_repo="gcr.io/google-samples"
-app_name="microservices-demo"
-app_version="v0.3.6" 
+source_repo=${ob_source_repo}
+app_name=${ob_app_name}
+app_version=${ob_app_version}
 
 registry_type=${1:?"Please specify ECR, GAR, or GCR"} 
 
 for container (adservice cartservice checkoutservice currencyservice emailservice frontend loadgenerator paymentservice productcatalogservice recommendationservice shippingservice ); do
 docker pull ${source_repo}/${app_name}/${container}:${app_version}
-#docker tag gcr.io/google-samples/microservices-demo/${container}:${version} ${aws_account}.dkr.ecr.${region}.amazonaws.com/microservices-demo/${container}:${version}
-#docker tag gcr.io/google-samples/microservices-demo/${container}:${version} ${region}-docker.pkg.dev/${gcp_project}/online-boutique/${container}:${version}
-#docker push ${aws_account}.dkr.ecr.${region}.amazonaws.com/microservices-demo/${container}:${version}
-#docker push ${region}-docker.pkg.dev/${gcp_project}/online-boutique/${container}:${version}
+
 case ${registry_type} in
 "ECR")
   # AWS ECR
