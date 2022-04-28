cf4c1856c5662d30cb229e872ddcbce461bc88ef
Mike Laramie <mike.laramie@lacework.net>
2022-04-28 12:43:32 -0400

    fix: fixed folders var

--- folders.tf
+++ folders.tf
@@ -1,6 +1,6 @@
 # Creates the folder(s) for our project(s) unless an existing folder is set via var.folder_id
 resource "google_folder" "main_folder" {
-  count        = var.org_id != null ? 1 : 0
+  count        = var.folder_id != null ? 1 : 0
   display_name = var.main_folder_name
   parent       = var.parent_folder != null ? "folders/${var.parent_folder}" : "organizations/${var.org_id}"
 }
